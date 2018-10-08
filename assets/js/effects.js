0fb3aa1ff5fe68969966fc498e1f1143887880a0
stegeri <stegerivan95@gmail.com>
2018-10-08 15:50:43 -0300

    Se agrego 'mi rutina'

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -9,3 +9,20 @@ function closeMenu(){
     a.style.left = "-75%";
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
+
+function showExercise(){
+    var acc = document.getElementsByClassName("dia");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+      });
+    }
+}
