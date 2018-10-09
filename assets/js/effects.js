ed542eef3ce65378aa5d5749d2afb7b989ebbf33
stegeri <stegerivan95@gmail.com>
2018-10-09 15:17:44 -0300

    Se agrego seccion de registro de ejercicio

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -26,3 +26,23 @@ function showExercise(){
       });
     }
 }
+
+
+//revisar
+$(document).ready(function(){
+    var i=3;
+    
+  $("#add_row").click(function(){                                                                                                                   //<td><input type='number' ></td>
+    $('#addr'+i).html("<th scope='row'>"+ (i+1) +"</th> <td><input type='number' name='rep"+ (i+1) +"' id='rep"+ (i+1) +"' min='1' max='100'></td> <td><input type='number' name='peso"+ (i+1) +"' id='peso"+ (i+1) +"' min='"+ (i+1) +"' max='100'></td>");
+    $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+    i++; 
+  });
+
+  $("#delete_row").click(function(){
+    if(i>3){
+          $("#addr"+(i-1)).html('');
+          i--;
+    }
+  });
+
+});
